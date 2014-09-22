da2afa3a484e1eebc7940b4fc017814267269c88
WoJoVo <woutervogelzang@gmail.com>
2014-09-22 11:38:39 +0200

    Comment GameEngine

--- Controller/GameEngine.cs
+++ Controller/GameEngine.cs
@@ -17,7 +17,7 @@ namespace WarGame.Controller
         {
             if (gameEngine == null)
                 gameEngine = new GameEngine();
-            //Hallo hallo
+            //Hallo hallo mitch
             return gameEngine;
         }
         
