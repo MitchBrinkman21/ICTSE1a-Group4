03e8747e305d87eea7ce094cb7d8e1ccf40fd981
WoJoVo <woutervogelzang@gmail.com>
2014-09-24 10:29:50 +0200

    Rectangle nu daadwerkelijk als property toegevoegd

--- Model/Player.cs
+++ Model/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,10 @@ namespace WarGame.Model
         public int x { get; set; }
         public int y { get; set; }
         public int speed { get; set; }
-        public int width;
-        public int length;
-        public string image;
+        public int width { get; set; }
+        public int length { get; set; }
+        public string image { get; set; }
+        public Rectangle rect { get; set; }
 
         public Player()
         {
@@ -23,6 +25,7 @@ namespace WarGame.Model
             this.speed = 10;
             this.width = 10;
             this.length = 10;
+            this.rect = new Rectangle(10, 10, 10, 10);
         }
 
         public void MovePlayer(int x, int y, int speed)
