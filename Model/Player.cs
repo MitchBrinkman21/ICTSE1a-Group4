b4c49b2193d090a1f8ae7dad9d47ded09a649e3a
WoJoVo <woutervogelzang@gmail.com>
2014-09-24 10:27:25 +0200

    Player attributen + methods toegevoegd, inclusief Rectangle

--- Model/Player.cs
+++ Model/Player.cs
@@ -8,12 +8,37 @@ namespace WarGame.Model
 {
     public class Player
     {
-        public int live;
-        public int x;
-        public int y;
-        public int speed;
+        public int lives { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int speed { get; set; }
         public int width;
-        public int lenght;
+        public int length;
+        public string image;
+
+        public Player()
+        {
+            this.x = 1;
+            this.y = 1;
+            this.speed = 10;
+            this.width = 10;
+            this.length = 10;
+        }
+
+        public void MovePlayer(int x, int y, int speed)
+        {
+            this.x = x;
+            this.y = y;
+            this.speed = speed;
+        }
+
+        public void DecreaseLive()
+        {
+            this.lives--;
+        } 
+
 
     }
+
+    
 }
