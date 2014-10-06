177f84158668dad40a793cad3c5c901bda52de85
Evan van Urk <EvanvanUrk@users.noreply.github.com>
2014-10-06 14:08:41 +0200

    Added buttonDelete to FormHighScores

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -202,6 +202,16 @@ namespace WarGame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap recycleBinSmall {
+            get {
+                object obj = ResourceManager.GetObject("recycleBinSmall", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap reset {
             get {
                 object obj = ResourceManager.GetObject("reset", resourceCulture);
--- View/FormHighScores.Designer.cs
+++ View/FormHighScores.Designer.cs
@@ -36,6 +36,8 @@
             this.columnHeaderPlace = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonDelete = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonDelete)).BeginInit();
             this.SuspendLayout();
             // 
             // labelLevel
@@ -43,18 +45,20 @@
             this.labelLevel.AutoSize = true;
             this.labelLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLevel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.labelLevel.Location = new System.Drawing.Point(25, 24);
+            this.labelLevel.Location = new System.Drawing.Point(19, 20);
+            this.labelLevel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelLevel.Name = "labelLevel";
-            this.labelLevel.Size = new System.Drawing.Size(52, 17);
+            this.labelLevel.Size = new System.Drawing.Size(42, 13);
             this.labelLevel.TabIndex = 0;
             this.labelLevel.Text = "Level:";
             // 
             // comboBoxLevel
             // 
             this.comboBoxLevel.FormattingEnabled = true;
-            this.comboBoxLevel.Location = new System.Drawing.Point(83, 24);
+            this.comboBoxLevel.Location = new System.Drawing.Point(62, 20);
+            this.comboBoxLevel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBoxLevel.Name = "comboBoxLevel";
-            this.comboBoxLevel.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxLevel.Size = new System.Drawing.Size(92, 21);
             this.comboBoxLevel.TabIndex = 1;
             this.comboBoxLevel.Text = "Select a level";
             this.comboBoxLevel.SelectedIndexChanged += new System.EventHandler(this.comboBoxLevel_SelectedIndexChanged);
@@ -66,9 +70,10 @@
             this.buttonOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonOK.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonOK.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.buttonOK.Location = new System.Drawing.Point(325, 374);
+            this.buttonOK.Location = new System.Drawing.Point(244, 304);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(61, 36);
+            this.buttonOK.Size = new System.Drawing.Size(46, 29);
             this.buttonOK.TabIndex = 3;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
@@ -82,9 +87,10 @@
             this.columnHeaderTime});
             this.listViewHighscores.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
             listViewItem1});
-            this.listViewHighscores.Location = new System.Drawing.Point(29, 77);
+            this.listViewHighscores.Location = new System.Drawing.Point(22, 63);
+            this.listViewHighscores.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listViewHighscores.Name = "listViewHighscores";
-            this.listViewHighscores.Size = new System.Drawing.Size(358, 268);
+            this.listViewHighscores.Size = new System.Drawing.Size(270, 218);
             this.listViewHighscores.TabIndex = 4;
             this.listViewHighscores.UseCompatibleStateImageBehavior = false;
             this.listViewHighscores.View = System.Windows.Forms.View.Details;
@@ -104,19 +110,32 @@
             this.columnHeaderTime.Text = "Time";
             this.columnHeaderTime.Width = 100;
             // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackgroundImage = global::WarGame.Properties.Resources.recycleBinSmall;
+            this.buttonDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonDelete.Location = new System.Drawing.Point(159, 17);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(19, 27);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.TabStop = false;
+            // 
             // FormHighScores
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(417, 422);
+            this.ClientSize = new System.Drawing.Size(313, 343);
+            this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.listViewHighscores);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.comboBoxLevel);
             this.Controls.Add(this.labelLevel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormHighScores";
             this.Text = "HighScores";
             this.Load += new System.EventHandler(this.FormHighScores_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.buttonDelete)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,5 +150,6 @@
         private System.Windows.Forms.ColumnHeader columnHeaderPlace;
         private System.Windows.Forms.ColumnHeader columnHeaderName;
         private System.Windows.Forms.ColumnHeader columnHeaderTime;
+        private System.Windows.Forms.PictureBox buttonDelete;
     }
 }
