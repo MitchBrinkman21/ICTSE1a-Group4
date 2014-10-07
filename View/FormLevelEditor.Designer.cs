7d5126f2d27b9ad90345d454fe12899cd59da7f8
Janjaap Ree <janjaapree@gmail.com>
2014-10-07 11:06:22 +0200

    - updated FormLevelEditor.Designer.cs

--- View/FormLevelEditor.Designer.cs
+++ View/FormLevelEditor.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.buttonLoad = new System.Windows.Forms.PictureBox();
-            this.buttonSave = new System.Windows.Forms.PictureBox();
-            this.buttonVisable = new System.Windows.Forms.PictureBox();
             this.panelToolBox = new System.Windows.Forms.Panel();
             this.panelRocketLauncher = new System.Windows.Forms.Panel();
             this.panelSandbag = new System.Windows.Forms.Panel();
@@ -39,49 +36,25 @@
             this.panelMud = new System.Windows.Forms.Panel();
             this.panelMine = new System.Windows.Forms.Panel();
             this.panelTree = new System.Windows.Forms.Panel();
+            this.buttonNew = new System.Windows.Forms.Panel();
+            this.buttonLoad = new System.Windows.Forms.Panel();
+            this.buttonSave = new System.Windows.Forms.Panel();
+            this.buttonVisable = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.buttonLoad)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.buttonSave)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.buttonVisable)).BeginInit();
             this.panelToolBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panelMenu.Controls.Add(this.buttonVisable);
+            this.panelMenu.Controls.Add(this.buttonNew);
             this.panelMenu.Controls.Add(this.buttonLoad);
             this.panelMenu.Controls.Add(this.buttonSave);
-            this.panelMenu.Controls.Add(this.buttonVisable);
             this.panelMenu.Location = new System.Drawing.Point(1217, 0);
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(148, 32);
             this.panelMenu.TabIndex = 1;
-            // 
-            // buttonLoad
-            // 
-            this.buttonLoad.Location = new System.Drawing.Point(21, 4);
-            this.buttonLoad.Name = "buttonLoad";
-            this.buttonLoad.Size = new System.Drawing.Size(25, 25);
-            this.buttonLoad.TabIndex = 3;
-            this.buttonLoad.TabStop = false;
-            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
-            // 
-            // buttonSave
-            // 
-            this.buttonSave.Location = new System.Drawing.Point(62, 4);
-            this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(25, 25);
-            this.buttonSave.TabIndex = 2;
-            this.buttonSave.TabStop = false;
-            // 
-            // buttonVisable
-            // 
-            this.buttonVisable.Location = new System.Drawing.Point(105, 4);
-            this.buttonVisable.Name = "buttonVisable";
-            this.buttonVisable.Size = new System.Drawing.Size(25, 25);
-            this.buttonVisable.TabIndex = 1;
-            this.buttonVisable.TabStop = false;
-            this.buttonVisable.Click += new System.EventHandler(this.buttonVisable_Click);
             // 
             // panelToolBox
             // 
@@ -143,7 +116,7 @@
             // 
             this.panelMine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.panelMine.BackgroundImage = global::WarGame.Properties.Resources.mine;
-            this.panelMine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelMine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panelMine.Location = new System.Drawing.Point(8, 16);
             this.panelMine.Name = "panelMine";
             this.panelMine.Size = new System.Drawing.Size(63, 63);
@@ -160,6 +133,36 @@
             this.panelTree.Size = new System.Drawing.Size(63, 63);
             this.panelTree.TabIndex = 2;
             this.panelTree.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Tree_MouseDown);
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(8, 3);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(25, 25);
+            this.buttonNew.TabIndex = 2;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(45, 3);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(25, 25);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(82, 3);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(25, 25);
+            this.buttonSave.TabIndex = 3;
+            // 
+            // buttonVisable
+            // 
+            this.buttonVisable.Location = new System.Drawing.Point(119, 3);
+            this.buttonVisable.Name = "buttonVisable";
+            this.buttonVisable.Size = new System.Drawing.Size(25, 25);
+            this.buttonVisable.TabIndex = 4;
+            this.buttonVisable.Click += new System.EventHandler(this.buttonVisable_Click);
             // 
             // FormLevelEditor
             // 
@@ -178,9 +181,6 @@
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FormLevelEditor_DragEnter);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormLevelEditor_Paint);
             this.panelMenu.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.buttonLoad)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.buttonSave)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.buttonVisable)).EndInit();
             this.panelToolBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -191,13 +191,14 @@
         private System.Windows.Forms.Panel panelToolBox;
         private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Panel panelTree;
-        private System.Windows.Forms.PictureBox buttonLoad;
-        private System.Windows.Forms.PictureBox buttonSave;
-        private System.Windows.Forms.PictureBox buttonVisable;
         private System.Windows.Forms.Panel panelRocketLauncher;
         private System.Windows.Forms.Panel panelSandbag;
         private System.Windows.Forms.Panel panelFinish;
         private System.Windows.Forms.Panel panelMud;
         private System.Windows.Forms.Panel panelMine;
+        private System.Windows.Forms.Panel buttonVisable;
+        private System.Windows.Forms.Panel buttonNew;
+        private System.Windows.Forms.Panel buttonLoad;
+        private System.Windows.Forms.Panel buttonSave;
     }
 }
