07daf438bec9b5bd7f214ddb6ea4c7a9bc4895c3
SanderBenjamins <sjanne1992@hotmail.com>
2014-10-08 11:47:41 +0200

    Missilelauncher.cs

---
+++ Model/Missilelauncher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame.Model
+{
+    public class Missilelauncher : Obstacle
+    {
+        public Missilelauncher(int x, int y)
+            : base(x, y)
+        {
+            width = 40;
+            length = 40;
+            rect = new Rectangle(x, y, width, length);
+            image = new Bitmap(WarGame.Properties.Resources.missilelauncher, width, length);
+        }
+    }
+}
