237836fe87e2f41eae874c26f5e66cec35932afd
Janjaap Ree <janjaapree@gmail.com>
2014-10-08 18:13:51 +0200

    fixed bug while parsing big level(doublebuffer)

--- View/FormImportLevelStatus.cs
+++ View/FormImportLevelStatus.cs
@@ -23,5 +23,14 @@ namespace WarGame.View
         {
             Close();
         }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
     }
 }
--- View/ProgressBarDialog.cs
+++ View/ProgressBarDialog.cs
@@ -37,5 +37,14 @@ namespace WarGame.View
         {
             progressBar.Value = value;
         }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
     }
 }
